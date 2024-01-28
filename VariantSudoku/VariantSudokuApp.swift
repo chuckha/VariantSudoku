69b26d0ece7670dc26dbedeb4200c03353926e0a
chuckha <chuckha@hey.com>
2024-01-28 11:03:50 -0800

    Initial Commit

---
+++ VariantSudoku/VariantSudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariantSudokuApp.swift
+//  VariantSudoku
+//
+//  Created by chuck ha on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct VariantSudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
